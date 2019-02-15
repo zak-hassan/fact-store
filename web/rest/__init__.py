8935e266b9f271bcc7e8237a95f103830c36d331
Zak Hassan <zak.hassan1010@gmail.com>
2019-02-14 21:18:35 -0500

    Setting up app

--- web/rest/__init__.py
+++ web/rest/__init__.py
@@ -1,9 +1,14 @@
-from flask import Flask, request
+from crypt import methods
+
+from flask import Flask, request, render_template
 app = Flask(__name__)
+import pyarrow.parquet as pq
+
+
 
 @app.route("/")
 def index():
-    return "Healthz"
+    return  render_template("index.html")
 
 @app.route("/anomaly")
 def anomaly():
@@ -15,17 +20,21 @@ def anomaly():
     return "anomaly"
 
 
-def persistMetadataToFactStore(id):
+def persistMetadataToFactStore(id, anomaly):
+    # TODO: Store results in parquet store
     pass
 
 
-@ app.route("/feedback")
+@ app.route("/feedback", methods=['GET'])
 def feedback():
     # TODO: persist to parquet file invalid anomaly
-    id = request.args.get('LAD_ID')
-    if id is None:
-        return "Must provide LAD_ID"
-    persistMetadataToFactStore(id)
+    id = request.args('LAD_ID')
+    anomaly = request.values.get('anomaly')
+
+    if id is None or anomaly is None:
+        return "Must provide LAD_ID or Anomaly parameter"
+    persistMetadataToFactStore(id,anomaly)
+    # TODO: Redirect user to confirmation page that we have logged the false anomaly.
     return "feedback+ "+id
 
 
