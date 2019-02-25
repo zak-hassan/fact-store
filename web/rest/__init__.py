3a7c50b2a5ead70f9cd6d350acd8f4dcf0ccebfc
Zak Hassan <zak.hassan1010@gmail.com>
2019-02-25 14:16:57 -0500

    providing webservice for log anomaly detection to poll for false predictions

--- web/rest/__init__.py
+++ web/rest/__init__.py
@@ -1,41 +1,68 @@
-import pandas as pd
-from fastparquet import write
 from flask import Flask, request, render_template
-app = Flask(__name__, static_folder="static")
-import s3fs
+import pandas as pd
+from fastparquet import write, ParquetFile
+import boto3
 import os
-import pyarrow.parquet as pq
+import datetime
 
-import pyarrow as pa
+PARQUET_FILE_NAME = 'factstore.parquet'
+app = Flask(__name__, static_folder="static")
+
 
 
 @app.route("/")
 def index():
     return  render_template("index.html")
 
+def syncModel():
+    """
+    Will check if the after training you would like to store models in s3
+    with timestamp. It will follow a particular pattern:
 
+    s3://<bucket_name>/<path>/<timestamp>
 
-def persistMetadataToFactStore(id, anomaly):
-    # TODO: Store results in parquet store
+    Later after running training you can serve models from a particular timestamp.
+
+    :return:
+    """
+
+    session = boto3.session.Session()
     s3_key = os.getenv("LAD_S3_KEY")
     s3_secret = os.getenv("LAD_S3_SECRET")
     s3_host = os.getenv("LAD_S3_HOST")
-    s3_bucket= os.getenv("LAD_S3_BUCKET")
-    client_kwargs =  {'endpoint_url': s3_host}
-    s3 = s3fs.S3FileSystem(key=s3_key,
-                           secret=s3_secret,
-                           client_kwargs=client_kwargs)
-    myopen = s3.open(s3_bucket+"/factstore.parquet","wb")
-    df = pd.DataFrame({'_id': [id], 'is_false_anomaly': [anomaly]})
-    ta = pa.Table.from_pandas(df)
-    pw = pq.ParquetWriter(myopen, schema=ta.schema)
-    pw.write_table(ta)
-    pw.close()
+    s3_client = session.client(service_name='s3',
+                               aws_access_key_id=s3_key,
+                               aws_secret_access_key=s3_secret,
+                               endpoint_url=s3_host)
+
+    #s3_bucket = os.getenv("LAD_S3_BUCKET")
+
+    s3_client.upload_file(Filename="factstore.parquet", Bucket="AIOPS", Key="anomaly-detection/metadata/factstore.parquet")
+    print.info("Done uploading models to s3 complete")
+
+def persistMetadataToFactStore(id, anomaly):
+    # TODO: Store results in parquet store
+    df = pd.DataFrame({'id': [id], 'is_false_anomaly': [anomaly],'date': [datetime.datetime.now()]})
+    if os.path.exists(PARQUET_FILE_NAME):
+        write(filename=PARQUET_FILE_NAME, data=df, append=True, compression='GZIP')
+    else:
+        write(filename=PARQUET_FILE_NAME, data=df, compression='GZIP')
+    print("Syncing factstore")
+    # TODO: Uncomment below so you can test out S3 integration.
+    #syncModel()
 
 
+
+@app.route("/api/metadata", methods=['GET'])
+def metadata():
+    """ Service to provide list of false anomalies to be relabeled during ml training run"""
+    pf = ParquetFile('factstore.parquet')
+    df = pf.to_pandas()
+    return df.to_json(orient='records')
 
 @app.route("/api/feedback", methods=['GET'])
 def feedback():
+    """ Feedback Service to provide user input on which false predictions this model provided."""
     id = request.args.get('lad_id')
     anomaly = request.args.get('false_anomaly')
     if id is None or anomaly is None:
